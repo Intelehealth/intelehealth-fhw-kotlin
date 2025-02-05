beaaf2f277dec5f1aed8d9a5d3df2a827bffccc7
Vaghela Mithun <mithun@intelehealth.org>
2025-02-05 19:35:33 +0530

    without detekt support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-import io.gitlab.arturbosch.detekt.Detekt
 
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -12,23 +11,23 @@ plugins {
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
-    alias(libs.plugins.detekt.plugin) apply false
+//    alias(libs.plugins.detekt.plugin) apply false
 }
 
-subprojects {
-    apply(plugin = Plugins.DETEKT)
-
-    tasks.withType<Detekt> {
-        config = rootProject.files("detekt/detekt.yml")
-        reports {
-            html {
-                required.set(true)
-                outputLocation.set(file("$rootDir/detekt/reports/detekt.html"))
-            }
-            xml {
-                required.set(true)
-                outputLocation.set(file("$rootDir/detekt/reports/detekt.xml"))
-            }
-        }
-    }
-}
+//subprojects {
+//    apply(plugin = Plugins.DETEKT)
+//
+//    tasks.withType<Detekt> {
+//        config = rootProject.files("detekt/detekt.yml")
+//        reports {
+//            html {
+//                required.set(true)
+//                outputLocation.set(file("$rootDir/detekt/reports/detekt.html"))
+//            }
+//            xml {
+//                required.set(true)
+//                outputLocation.set(file("$rootDir/detekt/reports/detekt.xml"))
+//            }
+//        }
+//    }
+//}
