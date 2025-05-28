534f39d445099e7f45687ba1c6d5e6c7864cb68d
hatanvirintele <mhasan@intelehealth.org>
2025-05-28 18:38:12 +0600

    Visit start date updated

--- common/src/main/java/org/intelehealth/common/utility/DateTimeUtils.java
+++ common/src/main/java/org/intelehealth/common/utility/DateTimeUtils.java
@@ -8,6 +8,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
@@ -35,6 +36,9 @@ public class DateTimeUtils {
     public static final String LAST_SYNC_DB_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final String LAST_SYNC_DISPLAY_FORMAT = "hh:mm a dd MMM yyyy";
     public static final String USER_DOB_DB_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+
+    //getting this format for visit
+    public static final String SERVER_FORMAT = "MMM dd, yyyy hh:mm:ss a";
             //1997-10-20T00:00:00.000+0530
 
     @SuppressLint("SimpleDateFormat")
@@ -148,4 +152,21 @@ public class DateTimeUtils {
     public static TimeZone getUTCTimeZone() {
         return TimeZone.getTimeZone(TIME_ZONE_UTC);
     }
+
+    public static String formatOneToAnother(String date, String sourceFormat, String anotherFormat) {
+        if(date==null || date.isEmpty()) return "";
+        String result = "";
+        SimpleDateFormat sdf;
+        SimpleDateFormat sdf1;
+
+        try {
+            sdf = new SimpleDateFormat(sourceFormat, Locale.ENGLISH);
+            sdf1 = new SimpleDateFormat(anotherFormat, Locale.ENGLISH);
+            result = sdf1.format(sdf.parse(date));
+        } catch (Exception e) {
+            return "";
+        }
+        return result;
+    }
+
 }
